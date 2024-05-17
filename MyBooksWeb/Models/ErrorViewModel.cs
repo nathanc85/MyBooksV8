db5e24454dc57c3c77847b439ddf2266d80da01f
nathanc85 <cnlnat@yahoo.com>
2024-05-17 15:53:08 -0400

    First commit: fresh MVC template project.

---
+++ MyBooksWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyBooksWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
