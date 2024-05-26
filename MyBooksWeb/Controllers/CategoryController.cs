05ce05b6c64886a4de60ca5552c7b6c73be06bf6
nathanc85 <cnlnat@yahoo.com>
2024-05-26 14:49:46 -0400

    Setup the Category Controller to connect to the db.

--- MyBooksWeb/Controllers/CategoryController.cs
+++ MyBooksWeb/Controllers/CategoryController.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBooksWeb.Data;
 
 namespace MyBooksWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            var categories = _db.Categories.ToList();
             return View();
         }
     }
