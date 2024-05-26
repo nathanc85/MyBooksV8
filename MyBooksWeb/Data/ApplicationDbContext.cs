120f7128f819a1d5614232774c7f1a6e397c9c94
nathanc85 <cnlnat@yahoo.com>
2024-05-26 13:07:24 -0400

    Setting EF Core and the Category Model.

---
+++ MyBooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MyBooksWeb.Models;
+
+namespace MyBooksWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
