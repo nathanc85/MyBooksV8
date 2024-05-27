7a1055c2b66295b6b60a7e58587be3eb55d065e0
nathanc85 <cnlnat@yahoo.com>
2024-05-27 13:15:52 -0400

    Created the Edit action method and display view

--- MyBooksWeb/Controllers/CategoryController.cs
+++ MyBooksWeb/Controllers/CategoryController.cs
@@ -35,5 +35,37 @@ namespace MyBooksWeb.Controllers
             
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if (category == null) {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name.ToLower() == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("", "Name and Display Order can not be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View();
+        }
     }
 }
