4b1a02b3cd52d0384455b9c8e0df50bac96a7446
nathanc85 <cnlnat@yahoo.com>
2024-05-27 01:16:46 -0400

    Starting the Create Category feature by adding the shell of the view and action method.

--- MyBooksWeb/Controllers/CategoryController.cs
+++ MyBooksWeb/Controllers/CategoryController.cs
@@ -16,5 +16,10 @@ namespace MyBooksWeb.Controllers
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
