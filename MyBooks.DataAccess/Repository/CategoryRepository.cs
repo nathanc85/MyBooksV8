03158eb0cb764ad9bebd426f5df6d0c706c47b69
nathanc85 <cnlnat@yahoo.com>
2024-05-28 20:36:50 -0400

    Implemented ICategoryRepository and CategoryRepository.

---
+++ MyBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using MyBooks.DataAccess.Data;
+using MyBooks.DataAccess.Repository.IRepository;
+using MyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ MyBooks.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBooks.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+        void Save();
+    }
+}
