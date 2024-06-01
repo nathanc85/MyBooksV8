8055d87495085ef9dad3a0b809f9dfea11931a75
nathanc85 <cnlnat@yahoo.com>
2024-06-01 17:57:41 -0400

    Added Rich Text to Description

--- MyBooksWeb/Program.cs
+++ MyBooksWeb/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area=Admin}/{controller=Category}/{action=Index}/{id?}");
+    pattern: "{area=Admin}/{controller=Product}/{action=Index}/{id?}");
 
 app.Run();
