8cc09e6ed213bded827681eab41e817fad5057af
nathanc85 <cnlnat@yahoo.com>
2024-06-01 17:02:27 -0400

    Combined Create and Update actions for Products

--- MyBooksWeb/Areas/Admin/Controllers/ProductController.cs
+++ MyBooksWeb/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace MyBooksWeb.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             // Get the list of categories.
             IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(c => new SelectListItem
@@ -38,10 +38,18 @@ namespace MyBooksWeb.Areas.Admin.Controllers
                 CategoryList = CategoryList
             };
 
-            return View(productVM);
+            if (id == null || id == 0) {
+                return View(productVM);
+            }
+            else
+            {
+                Product product = _unitOfWork.Product.Get(p => p.Id == id);
+                productVM.Product = product;
+                return View(productVM);
+            }
         }
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -68,35 +76,80 @@ namespace MyBooksWeb.Areas.Admin.Controllers
             }
         }
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
-            //Product? productFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+        //public IActionResult Create()
+        //{
+        //    // Get the list of categories.
+        //    IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(c => new SelectListItem
+        //    {
+        //        Text = c.Name,
+        //        Value = c.Id.ToString()
+        //    });
 
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
+        //    ProductVM productVM = new ProductVM()
+        //    {
+        //        Product = new Product(),
+        //        CategoryList = CategoryList
+        //    };
 
-        }
+        //    return View(productVM);
+        //}
+        //[HttpPost]
+        //public IActionResult Create(ProductVM productVM)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Product.Add(productVM.Product);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Product created successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    else
+        //    {
+        //        // Get the list of categories.
+        //        IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(c => new SelectListItem
+        //        {
+        //            Text = c.Name,
+        //            Value = c.Id.ToString()
+        //        });
+
+        //        productVM = new ProductVM()
+        //        {
+        //            CategoryList = CategoryList
+        //        };
+
+        //        return View(productVM);
+        //    }
+        //}
+
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+        //    //Product? productFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+        //    //Product? productFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+        //    if (productFromDb == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(productFromDb);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(Product obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Product.Update(obj);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Product updated successfully";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+
+        //}
 
         public IActionResult Delete(int? id)
         {
